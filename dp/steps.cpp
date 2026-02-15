35ec5140cfb27461f6078d6049bb8dd6d4fa999b
LOSt-GENZ <daringdumplings@gmail.com>
2026-02-15 17:38:09 +0530

    Climbing Stairs
    use the concept of Fibonacci and recursion

---
+++ dp/steps.cpp
@@ -0,0 +1,35 @@
+//you have n steps to climb to the top
+//you can climb either 1 or two steps
+//for eg:- if n is 2 u can take 1+1 step or 2 steps-so output is 2
+//lets say n=3, then you can take 1+1+1,1+2,2+1 steps-so output is 3
+//basically expressing in how many number of ways I can write it as a sum using 1's and 2's
+//if number of steps is equal to zero, the return 1
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int climbStairs(int n){
+
+    if(n==0)
+        return 1;
+
+    if(n==1)
+        return 1;
+
+    return climbStairs(n-1)+climbStairs(n-2);
+
+
+}
+
+int main(){
+
+    cout<<"Enter the number of steps"<<endl;
+    int n;
+    cin>>n;
+    cout<<climbStairs(n)<<endl;
+    return 0;
+
+
+}
