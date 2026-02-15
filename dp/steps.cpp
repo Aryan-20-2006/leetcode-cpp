59e0976003a2b8af36c655d5e3b964abd13e04ba
LOSt-GENZ <daringdumplings@gmail.com>
2026-02-15 18:12:07 +0530

    Fix:Climbing Stairs
    Used the concept of indexing to optimize the solution

--- dp/steps.cpp
+++ dp/steps.cpp
@@ -10,18 +10,43 @@
 using namespace std;
 
 
+// int climbStairs(int n){
+
+//     if(n==0)
+//         return 1;
+
+//     if(n==1)
+//         return 1;
+
+//     return climbStairs(n-1)+climbStairs(n-2);
+
+
+// }
+
+//optimized version-write in terms of indexes
+
 int climbStairs(int n){
 
-    if(n==0)
-        return 1;
+    if(n<=1)
+        return 1; //this includes for both n==0 and n==1
 
-    if(n==1)
-        return 1;
+    vector<int>dp(n+1); //this is initialzed with zero
+    dp[0]=1;
+    dp[1]=1;
 
-    return climbStairs(n-1)+climbStairs(n-2);
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+
+    return dp[n];
 
 
 }
+
+
+
+
+
 
 int main(){
 
