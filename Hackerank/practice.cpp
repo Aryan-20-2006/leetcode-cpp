7b32bc7b44afd93ae2b5e9fede84ea43b18ec754
LOSt-GENZ <daringdumplings@gmail.com>
2026-06-25 12:05:42 +0530

    Practicing rom HR

--- Hackerank/practice.cpp
+++ Hackerank/practice.cpp
@@ -32,35 +32,107 @@ using namespace std;
 // }
 
 //Counting Valleys
-int countingValleys(int steps, string path){
+// int countingValleys(int steps, string path){
 
-    int altitude=0;
-    int valleys=0;
+//     int altitude=0;
+//     int valleys=0;
 
-    for(int i=0;i<path.size();i++){
-        if(path[i]=='U'){
-            altitude+=1;
-        }
+//     for(int i=0;i<path.size();i++){
+//         if(path[i]=='U'){
+//             altitude+=1;
+//         }
 
-        else
-            altitude-=1;
+//         else
+//             altitude-=1;
 
-        if(path[i]=='U' && altitude==0){
-            valleys++;
-        }
+//         if(path[i]=='U' && altitude==0){
+//             valleys++;
+//         }
 
+//     }
+
+// return valleys;
+
+// }
+
+
+// int main(){
+
+//     string path="DDUU";
+//     int steps=4;
+//     cout<<countingValleys(steps,path)<<" ";
+//     return 0;
+
+// }
+
+// int jumpingOnClouds(vector<int>&c){
+
+//     int jumps=0;
+//     int i=0;
+
+//     int n=c.size();
+
+//     while(i<n-1){
+
+//         if(i+2<n && c[i+2]==0){
+//             i+=2;
+//         }
+        
+//         else{
+//             i+=1;
+//         }
+
+
+//         jumps++;
+//     }
+
+//     return jumps;
+    
+// }
+
+// int main(){
+
+//     vector<int>c={0,1,0,0,0,1,0};
+//     cout<<jumpingOnClouds(c)<<" ";
+//     return 0;
+
+// }
+
+//Repeated String
+long repeatedString(string s,long n){
+
+    int count=0;
+    int ans;
+
+    for(int i=0;i<s.size();i++){
+        
+        if(s[i]=='a')
+            count++;
     }
 
-return valleys;
+    //Instead of repeating the string, how may complete copues of s fit into n
+    int completeCopies=n/s.size();
 
+    ans=count*completeCopies;
+
+    int rem=n%s.size();
+
+    //count remaining characters
+    for(int i=0;i<rem;i++){
+        if(s[i]=='a'){
+            ans++;
+        }
+    }
+
+return ans;
+    
 }
-
 
 int main(){
 
-    string path="DDUU";
-    int steps=4;
-    cout<<countingValleys(steps,path)<<" ";
+    string s="aba";
+    long n=10;
+    cout<<repeatedString(s,n)<<" ";
     return 0;
 
 }
